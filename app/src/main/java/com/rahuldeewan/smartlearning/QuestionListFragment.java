700d9512c4714810e68a2551beb1723f6f09923d
Rahul Deewan <rahuldeewanrd@gmail.com>
2017-11-09 21:56:49 +0530

    Added Custom Dialog

--- app/src/main/java/com/rahuldeewan/smartlearning/QuestionListFragment.java
+++ app/src/main/java/com/rahuldeewan/smartlearning/QuestionListFragment.java
@@ -5,13 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.logging.Logger;
 
-public class QuestionListFragment extends Fragment {
+public class QuestionListFragment extends Fragment implements View.OnClickListener {
     private static final String QUESTION_NO = "id";
     private static final String QUESTION = "question";
     private static final String OPTIONA = "optionA";
@@ -22,21 +20,25 @@ public class QuestionListFragment extends Fragment {
     private static final String SOLUTION = "solution";
     private static final String ANSWER = "answer";
 
+    private TextView tvQuestionNo;
+    private TextView tvQuestion;
+    private TextView tvOptionA;
+    private TextView tvOptionB;
+    private TextView tvOptionC;
+    private TextView tvOptionD;
+    private TextView tvHint;
+    private TextView tvSolution;
+    private TextView tvAnswer;
+    private CustomDialog customDialog;
     private Logger logger = Logger.getLogger("QuestionListFragment");
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-//    private OnFragmentInteractionListener mListener;
 
     public QuestionListFragment() {
     }
 
-    public static QuestionListFragment newInstance(int position,Question question) {
+    public static QuestionListFragment newInstance(int position, Question question) {
         QuestionListFragment fragment = new QuestionListFragment();
         Bundle args = new Bundle();
-        args.putString(QUESTION_NO, "Question "+String.valueOf(position));
+        args.putString(QUESTION_NO, "Question " + String.valueOf(position));
         args.putString(QUESTION, question.getQuestion());
         args.putString(OPTIONA, question.getOptionA());
         args.putString(OPTIONB, question.getOptionB());
@@ -53,15 +55,19 @@ public class QuestionListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_question_list, container, false);
-        TextView tvQuestionNo = rootView.findViewById(R.id.tv_question_no);
-        TextView tvQuestion = rootView.findViewById(R.id.tv_question);
-        TextView tvOptionA = rootView.findViewById(R.id.tv_option_a);
-        TextView tvOptionB = rootView.findViewById(R.id.tv_option_b);
-        TextView tvOptionC = rootView.findViewById(R.id.tv_option_c);
-        TextView tvOptionD = rootView.findViewById(R.id.tv_option_d);
-        TextView tvAnswer = rootView.findViewById(R.id.tv_answer);
-        TextView tvHint = rootView.findViewById(R.id.tv_hint);
-        TextView tvSolution = rootView.findViewById(R.id.tv_solution);
+        tvQuestionNo = rootView.findViewById(R.id.tv_question_no);
+        tvQuestion = rootView.findViewById(R.id.tv_question);
+        tvOptionA = rootView.findViewById(R.id.tv_option_a);
+        tvOptionB = rootView.findViewById(R.id.tv_option_b);
+        tvOptionC = rootView.findViewById(R.id.tv_option_c);
+        tvOptionD = rootView.findViewById(R.id.tv_option_d);
+        tvAnswer = rootView.findViewById(R.id.tv_answer);
+        tvHint = rootView.findViewById(R.id.tv_hint);
+        tvSolution = rootView.findViewById(R.id.tv_solution);
+
+        tvHint.setOnClickListener(this);
+        tvSolution.setOnClickListener(this);
+        tvAnswer.setOnClickListener(this);
 
         tvQuestionNo.setText(getArguments().getString(QUESTION_NO));
         tvQuestion.setText(getArguments().getString(QUESTION));
@@ -69,49 +75,23 @@ public class QuestionListFragment extends Fragment {
         tvOptionB.setText(getArguments().getString(OPTIONB));
         tvOptionC.setText(getArguments().getString(OPTIONC));
         tvOptionD.setText(getArguments().getString(OPTIOND));
-//        tvHint.setText(getArguments().getString(HINT));
-//        tvSolution.setText(getArguments().getString(SOLUTION));
-//        tvAnswer.setText(getArguments().getString(ANSWER));
         return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.tv_hint) {
+            customDialog = new CustomDialog(getActivity(), "Hint", getArguments().getString(HINT));
+            customDialog.show();
+        }
+        if (view.getId() == R.id.tv_solution) {
+            customDialog = new CustomDialog(getActivity(), "Solution", getArguments().getString(SOLUTION));
+            customDialog.show();
+        }
+        if (view.getId() == R.id.tv_answer) {
+            customDialog = new CustomDialog(getActivity(), "Answer", getArguments().getString(ANSWER));
+            customDialog.show();
+        }
 
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+    }
 }
